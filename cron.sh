55952342fd5e579750690bc1536ea678fea46c71
Bruno Binet <bruno.binet@gmail.com>
2025-09-26 16:32:14 +0200

    Add cron.sh script

---
+++ cron.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+(cd /home/acs-technolac/acs_helloasso_show_memberships;
+    cp acs.json acs.json.old;
+    npm run acsdata;
+    cmp -s acs.json acs.json.old && exit 0;
+    npm run build;
+
+)
+(cd /home/acs-technolac/acs_helloasso_show_memberships/gh-pages;
+    cp ../dist/index.html index.html;
+    git commit -a -m "Update index.html" && git push || echo "No changes to commit"
+)
