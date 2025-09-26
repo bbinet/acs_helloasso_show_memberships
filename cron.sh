2f143e758bfedad55daf5975884eeb24b337b2ef
Bruno Binet <bruno.binet@gmail.com>
2025-09-26 16:44:31 +0200

    Update cron.sh script

--- cron.sh
+++ cron.sh
@@ -3,11 +3,11 @@
 (cd /home/acs-technolac/acs_helloasso_show_memberships;
     cp acs.json acs.json.old;
     npm run acsdata;
-    cmp -s acs.json acs.json.old && exit 0;
+    cmp -s acs.json acs.json.old && echo "Nothing to do." && exit 0;
     npm run build;
 
-)
-(cd /home/acs-technolac/acs_helloasso_show_memberships/gh-pages;
-    cp ../dist/index.html index.html;
-    git commit -a -m "Update index.html" && git push || echo "No changes to commit"
+    (cd /home/acs-technolac/acs_helloasso_show_memberships/gh-pages;
+        cp ../dist/index.html index.html;
+        git commit -a -m "Update index.html" && git push
+    )
 )
